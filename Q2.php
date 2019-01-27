d6d0df3b9f3865b30e56492da2c5bb005783375b
k-hiroo <grieg.1686@gmail.com>
2019-01-28 01:18:57 +0900

    Q2 modify 20190128

--- Q2.php
+++ Q2.php
@@ -10,18 +10,14 @@ for ($i = 1000; $i < 10000; $i ++) {
 //理解しやすさ重視
 if($i == strrev(substr($i, 0, 1) * substr($i, 1, 1) * substr($i, 2, 1)*substr($i, 3, 1))){
   echo $i;
-}
-if($i == strrev(substr($i, 0, 1) * (int)ltrim((string)substr($i, 1, 3),'0') )){
+}if($i == strrev(substr($i, 0, 1) * (int)ltrim((string)substr($i, 1, 3),'0') )){
   echo $i;
 }if($i == strrev((int)ltrim((string)substr($i, 0, 3),'0') * substr($i, 3, 1))){
   echo $i;
-}if
-($i == strrev((int)ltrim((string)substr($i, 0, 2),'0') * substr($i, 2, 1) * substr($i, 3, 1))){
+}if($i == strrev((int)ltrim((string)substr($i, 0, 2),'0') * substr($i, 2, 1) * substr($i, 3, 1))){
   echo $i;
-}if
-($i == strrev(substr($i, 0, 1) * (int)ltrim((string)substr($i, 1, 2),'0') * substr($i, 3, 1))){
+}if($i == strrev(substr($i, 0, 1) * (int)ltrim((string)substr($i, 1, 2),'0') * substr($i, 3, 1))){
   echo $i;
-}if
-($i == strrev(substr($i, 0, 1) * substr($i, 1, 1) * (int)ltrim((string)substr($i, 2, 2),'0'))){
+}if($i == strrev(substr($i, 0, 1) * substr($i, 1, 1) * (int)ltrim((string)substr($i, 2, 2),'0'))){
   echo $i;
 }
